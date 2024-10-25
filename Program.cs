f99d0c36b263bd13437fb1d197fa642bfdc2b1ff
Alexandre Oliveira Nunes <alexandre_nunes@Alexandre.home>
2024-10-25 13:18:18 -0300

    trilha-net-poo-desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
+Iphone iphone = new Iphone("16996256478","Iphone 8 Plus","123",264);
+Nokia nokia= new Nokia("16996256479","Nokia 3","123",500);
+
+iphone.InstalarAplicativo("Lichess");
+nokia.InstalarAplicativo("Lichess");
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
